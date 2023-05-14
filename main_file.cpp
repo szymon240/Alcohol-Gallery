428d74876ba891cd13a2e0c1573ec6b77ad4d727
Szymon <s.szymankiewicz@icloud.com>
2023-05-14 20:10:56 +0200

    usuwanko

--- main_file.cpp
+++ main_file.cpp
@@ -1,19 +1,3 @@
-/*
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-
 #define GLM_FORCE_RADIANS
 
 #include <GL/glew.h>
@@ -30,25 +14,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "myTeapot.h"
 #include <iostream>
 
-float speed_x=0; //angular speed in radians
-float speed_y=0; //angular speed in radians
-float aspectRatio=1;
-ShaderProgram *sp; //Pointer to the shader program
 
-
-//Uncomment to draw a cube
-/*float* vertices=myCubeVertices;
-float* texCoords= myCubeTexCoords;
-float* colors = myCubeColors;
-float* normals = myCubeNormals;
-int vertexCount = myCubeVertexCount;*/
-
-//Uncomment to draw a teapot
-float* vertices = myTeapotVertices;
-float* texCoords = myTeapotTexCoords;
-float* colors = myTeapotColors;
-float* normals = myTeapotVertexNormals;
-int vertexCount = myTeapotVertexCount;
 
 //Error processing callback procedure
 void error_callback(int error, const char* description) {
@@ -56,89 +22,32 @@ void error_callback(int error, const char* description) {
 }
 
 void keyCallback(GLFWwindow* window,int key,int scancode,int action,int mods) {
-    if (action==GLFW_PRESS) {
-        if (key==GLFW_KEY_LEFT) speed_x=-PI/2;
-        if (key==GLFW_KEY_RIGHT) speed_x=PI/2;
-        if (key==GLFW_KEY_UP) speed_y=PI/2;
-        if (key==GLFW_KEY_DOWN) speed_y=-PI/2;
-    }
-    if (action==GLFW_RELEASE) {
-        if (key==GLFW_KEY_LEFT) speed_x=0;
-        if (key==GLFW_KEY_RIGHT) speed_x=0;
-        if (key==GLFW_KEY_UP) speed_y=0;
-        if (key==GLFW_KEY_DOWN) speed_y=0;
-    }
-}
-
-void windowResizeCallback(GLFWwindow* window,int width,int height) {
-    if (height==0) return;
-    aspectRatio=(float)width/(float)height;
-    glViewport(0,0,width,height);
 }
 
 
 //Initialization code procedure
 void initOpenGLProgram(GLFWwindow* window) {
-	//************Place any code here that needs to be executed once, at the program start************
 	glClearColor(0,0,0,1);
 	glEnable(GL_DEPTH_TEST);
-	glfwSetWindowSizeCallback(window,windowResizeCallback);
 	glfwSetKeyCallback(window,keyCallback);
-	sp=new ShaderProgram("v_simplest.glsl",NULL,"f_simplest.glsl");
-
 }
 
 //Release resources allocated by the program
 void freeOpenGLProgram(GLFWwindow* window) {
 	//************Place any code here that needs to be executed once, after the main loop ends************
-	delete sp;
 }
 
-//Drawing procedure
-void drawScene(GLFWwindow* window,float angle_x,float angle_y) {
-	//************Place any code here that draws something inside the window******************l
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void drawScene(GLFWwindow* window) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // SPRAWDZIÆ!!
 
-	glm::mat4 V=glm::lookAt(
-        glm::vec3(0.0f,0.0f,-3.0f),
-        glm::vec3(0.0f,0.0f,0.0f),
-        glm::vec3(0.0f,1.0f,0.0f)); //compute view matrix
-    glm::mat4 P=glm::perspective(50.0f*PI/180.0f, 1.0f, 1.0f, 50.0f); //compute projection matrix
-
-    sp->use();//activate shading program
-    //Send parameters to graphics card
-    glUniformMatrix4fv(sp->u("P"),1,false,glm::value_ptr(P));
-    glUniformMatrix4fv(sp->u("V"),1,false,glm::value_ptr(V));
-
-    glm::mat4 M=glm::mat4(1.0f);
-	M=glm::rotate(M,angle_y,glm::vec3(1.0f,0.0f,0.0f)); //Compute model matrix
-	M=glm::rotate(M,angle_x,glm::vec3(0.0f,1.0f,0.0f)); //Compute model matrix
-    glUniformMatrix4fv(sp->u("M"),1,false,glm::value_ptr(M));
-
-
-    glEnableVertexAttribArray(sp->a("vertex")); //Enable sending data to the attribute vertex
-    glVertexAttribPointer(sp->a("vertex"),4,GL_FLOAT,false,0,vertices); //Specify source of the data for the attribute vertex
-
-	glEnableVertexAttribArray(sp->a("color")); //Enable sending data to the attribute color
-	glVertexAttribPointer(sp->a("color"), 4, GL_FLOAT, false, 0, colors); //Specify source of the data for the attribute color
-
-	glEnableVertexAttribArray(sp->a("normal")); //Enable sending data to the attribute color
-	glVertexAttribPointer(sp->a("normal"), 4, GL_FLOAT, false, 0, normals); //Specify source of the data for the attribute normal
-
-    glDrawArrays(GL_TRIANGLES,0,vertexCount); //Draw the object
-
-    glDisableVertexAttribArray(sp->a("vertex")); //Disable sending data to the attribute vertex
-	glDisableVertexAttribArray(sp->a("color")); //Disable sending data to the attribute color
-	glDisableVertexAttribArray(sp->a("normal")); //Disable sending data to the attribute normal
-
-    glfwSwapBuffers(window); //Copy back buffer to front buffer
+	glfwSwapBuffers(window); //Copy back buffer to front buffer
 }
 
 int main(void)
 {
 	GLFWwindow* window; //Pointer to object that represents the application window
-	std::cout << "d";
+
 	glfwSetErrorCallback(error_callback);//Register error processing callback procedure
 
 	if (!glfwInit()) { //Initialize GLFW library
@@ -146,7 +55,7 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	window = glfwCreateWindow(500, 500, "OpenGL", NULL, NULL);  //Create a window 500pxx500px titled "OpenGL" and an OpenGL context associated with it.
+	window = glfwCreateWindow(1280, 720, "OpenGL", NULL, NULL);  //Rozmiar HD (rozdzielczoœæ)   SPRAWDZIÆ NULLE!   Create a window 500pxx500px titled "OpenGL" and an OpenGL context associated with it.
 
 	if (!window) //If no window is opened then close the program
 	{
@@ -166,17 +75,11 @@ int main(void)
 	initOpenGLProgram(window); //Call initialization procedure
 
 
-	float angle_x=0; //current rotation angle of the object, x axis
-	float angle_y=0; //current rotation angle of the object, y axis
-	glfwSetTime(0); //Zero the timer
 	//Main application loop
 	while (!glfwWindowShouldClose(window)) //As long as the window shouldnt be closed yet...
 	{
-        angle_x+=speed_x*glfwGetTime(); //Add angle by which the object was rotated in the previous iteration
-		angle_y+=speed_y*glfwGetTime(); //Add angle by which the object was rotated in the previous iteration
-        glfwSetTime(0); //Zero the timer
-		drawScene(window,angle_x,angle_y); //Execute drawing procedure
-		glfwPollEvents(); //Process callback procedures corresponding to the events that took place up to now
+		drawScene(window); //Execute drawing procedure
+		glfwPollEvents(); //Process callback procedures corresponding to the events that took place up to now   SPRAWDZIÆ!!
 	}
 	freeOpenGLProgram(window);
 
