fb88e668122f51afd8d9d26c3ddda40f0fe7c7c9
Szymon <s.szymankiewicz@icloud.com>
2023-06-04 11:12:27 +0200

    butelki

--- Scene.cpp
+++ Scene.cpp
@@ -31,15 +31,24 @@ void Scene::addObject(std::unique_ptr<WorldObject> ob) {
 }
 
 void Scene::loadLevel() {
-	std::unique_ptr<WorldObject> ob = std::make_unique<WorldObject>("objects/Cubone/modell.obj", "objects/metal.png");
+	
+	std::unique_ptr<WorldObject> ob = std::make_unique<WorldObject>("objects/potion/potion.obj", glm::vec3(-2.0f, 0.0f, 1.0f), "objects/metal.png");
 	objects.push_back(std::move(ob));
 
-	std::unique_ptr<WorldObject> ob1 = std::make_unique<WorldObject>("objects/Cubone/modell.obj", glm::vec3(0.5f, 0.0f, 1.0f), "objects/bricks.png");
+	std::unique_ptr<WorldObject> ob1 = std::make_unique<WorldObject>("objects/butelka/butelka.obj", glm::vec3(0.0f, -0.5f, 1.0f), "objects/metal.png");
 	objects.push_back(std::move(ob1));
 
-	// Uncomment the following lines if you want to add another object
-	// std::unique_ptr<WorldObject> ob2 = std::make_unique<WorldObject>("objects/Cubone/modell.obj", glm::vec3(-3.0f, 0.0f, 1.0f), "objects/bricks.png");
-	// objects.push_back(std::move(ob2));
+	std::unique_ptr<WorldObject> ob2 = std::make_unique<WorldObject>("objects/wine/wine2.obj", glm::vec3(2.0f, -0.5f, 1.0f), "objects/metal.png");
+	objects.push_back(std::move(ob2));
+
+	std::unique_ptr<WorldObject> ob3 = std::make_unique<WorldObject>("objects/potion/potion.obj", glm::vec3(-2.0f, 0.0f, -1.0f), "objects/metal.png");
+	objects.push_back(std::move(ob3));
+
+	std::unique_ptr<WorldObject> ob4 = std::make_unique<WorldObject>("objects/butelka/butelka.obj", glm::vec3(0.0f, -0.5f, -1.0f), "objects/metal.png");
+	objects.push_back(std::move(ob4));
+
+	std::unique_ptr<WorldObject> ob5 = std::make_unique<WorldObject>("objects/wine/wine2.obj", glm::vec3(2.0f, -0.5f, -1.0f), "objects/metal.png");
+	objects.push_back(std::move(ob5));
 }
 void Scene::clean()
 {
